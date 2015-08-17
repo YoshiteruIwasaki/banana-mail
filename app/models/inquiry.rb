68c3c8d77376f9c0687942506c14e94983a18c4b
YoshiteruIwasaki <yoshiteru.i@gmail.com>
2015-08-18 00:00:20 +0900

    順番変更

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,10 +1,10 @@
 class Inquiry < ActiveRecord::Base
   validates :name, presence: true,length: { maximum: 100 }
-  validates :address1, presence:true,length: { maximum: 250 }
-  validates :email, presence:true,email: true, length: { maximum: 250 }
-  validates :content, presence: true, length: { maximum: 10000 }
   validates :zip, length: { is: 7 }
   validates :prefecture, numericality: {
             only_integer: true, greater_than_or_equal_to: 0,less_than_or_equal_to: 50
           }
+  validates :address1, presence:true,length: { maximum: 250 }
+  validates :email, presence:true,email: true, length: { maximum: 250 }
+  validates :content, presence: true, length: { maximum: 10000 }
 end
